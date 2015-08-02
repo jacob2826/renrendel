f8b45800fbebe68c6a83ba3484c049eaaac21f68
panfake <wanni@vip.qq.com>
2015-08-02 19:38:20 +0800

    1.修改错误2.增加循环自动删除
    1.修改错误2.增加循环自动删除

--- renwa.py
+++ renwa.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-  
+# -*- coding:utf-8 -*-
 #按页删除人人网状态程序
 
 import urllib
@@ -6,61 +6,57 @@ import urllib2
 import cookielib
 import re
 
-#初始化一个CookieJar来处理Cookie的信息#  
+
+max_pagenums = input("Please input how many pages:\n")
+print ">>>>>>The max_pagenums is" , max_pagenums
+
+#初始化一个CookieJar来处理Cookie的信息#
 cookie = cookielib.CookieJar()
-
-#创建一个新的opener来使用我们的CookieJar#  
+#创建一个新的opener来使用我们的CookieJar#
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-
 #提交用户名密码
 postdata = urllib.urlencode({
-    'email':'',
-    'password':'',
-
+    'email':'YOUR USERNAME',
+    'password':'YOUR PASSWORD',
+})
 #模拟登录
 req = urllib2.Request(
     url = 'http://m.renren.com/login.do',
     data = postdata
-
 )
 result = opener.open(req).read()
 
 #获取sid,rtk
 resultsid = re.findall(r'sid=(.*?)&',result)
-
 renrensid = resultsid[0]
 print renrensid
 resultrtk = re.findall(r'_rtk=(.*?)&',result)
 renrenrtk = resultrtk[0]
 print renrenrtk
 
-#获取状态页
-statusurl = 'http://3g.renren.com/status/getdoing.do?&sid='+renrensid
-statuspage = opener.open(statusurl).read()
-print statuspage
+for i in range(0, max_pagenums-1):
+    #获取状态页
+    statusurl = 'http://3g.renren.com/status/getdoing.do?&sid='+renrensid+'curpage='+str(i)
+    statuspage = opener.open(statusurl).read()
+    print statuspage
 
-#获取状态ID：
-resultname = re.findall(r'a name..(\d*?).>',statuspage)
-print resultname
+    #获取状态ID：
+    resultname = re.findall(r'a name..(\d*?).>',statuspage)
+    print resultname
 
-postdel = urllib.urlencode({
-    'sid':renrensid,
-    '_rtk':renrenrtk,
-    
-})
+    postdel = urllib.urlencode({
+        'sid':renrensid,
+        '_rtk':renrenrtk,
 
-for renrenname in resultname:
-    print renrenname
-    statusurll = 'http://3g.renren.com/status/wdelstatus.do?id='+renrenname
-    reqdel = urllib2.Request(
-        url = statusurll,
-        data = postdel
+    })
 
-    )
-    print reqdel
-    resultdel = opener.open(reqdel).read()
+    for renrenname in resultname:
+        print renrenname
+        statusurll = 'http://3g.renren.com/status/wdelstatus.do?id='+renrenname
+        reqdel = urllib2.Request(
+            url = statusurll,
+            data = postdel
 
-  
-    
-
-
+        )
+        print reqdel
+        resultdel = opener.open(reqdel).read()
